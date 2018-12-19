44c5033f319c03b9ff0c6b14e02fd7b9a2c7bd1e
barbichola0899 <barbichola0899@users.noreply.github.com>
2018-12-19 18:39:59 -0200

    new event

--- bot.py
+++ bot.py
@@ -55,41 +55,41 @@ async def on_message(message):
         else:
             await client.send_message(message.channel," ``` %s = %i. %s```"  %(dice,s,rolls))
         
-        if message.content.lower().startswith('-r'):
-        args = message.content.split(" ")
-        #args[0] = -r
-        #args[1] = 1d6
-        dice_mod = args[len(args)-1]
-        try:
-            dice = dice_mod.split('+')[0]
-        except ValueError:
-            dice = dice_mod
-        #dice[0] = 1
-        #dice[2] = 6
-        i = 0
-        qtd,size_mod = dice_mod.split('d')
-        qtd = int(qtd)
-        try:
-            size,mod = size_mod.split('+')
+        if message.content.lower().startswith('-ra'):
+            args = message.content.split(" ")
+            #args[0] = -r
+            #args[1] = 1d6
+            dice_mod = args[len(args)-1]
+            try:
+                dice = dice_mod.split('+')[0]
+            except ValueError:
+                dice = dice_mod
+            #dice[0] = 1
+            #dice[2] = 6
+            i = 0
+            qtd,size_mod = dice_mod.split('d')
+            qtd = int(qtd)
+            try:
+                size,mod = size_mod.split('+')
 
-        except ValueError:
-            size = size_mod
-            mod = 0
-        
-        size = int(size)
-        mod = int(mod)
-        rolls = []
-        s = 0
-        for i in range(qtd):
-            roll = random.randint(1,size)
-            rolls.append(roll)
-            s += roll
-        s+=mod
-        rolls = str(rolls)
-        if mod != 0:
-            await client.send_message(message.channel," ``` %s + %i = %i. %s```"  %(dice,mod,s,rolls))
-        else:
-            await client.send_message(message.channel," ``` %s = %i. %s```"  %(dice,s,rolls))
+            except ValueError:
+                size = size_mod
+                mod = 0
+            
+            size = int(size)
+            mod = int(mod)
+            rolls = []
+            s = 0
+            for i in range(qtd):
+                roll = random.randint(1,size)
+                rolls.append(roll)
+                s += roll
+            s+=mod
+            rolls = str(rolls)
+            if mod != 0:
+                await client.send_message(message.channel," ``` %s + %i = %i. %s```"  %(dice,mod,s,rolls))
+            else:
+                await client.send_message(message.channel," ``` %s = %i. %s```"  %(dice,s,rolls))
 
 
 client.run(token) #Replace token with your bots token
